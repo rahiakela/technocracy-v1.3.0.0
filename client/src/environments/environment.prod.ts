7eb8be99ae9e9a490bb80f01170a928ff0479be9
tecknocracy <editors@tecknocracy.com>
2018-11-09 12:45:50 +0530

    Some PWA twiking,  blog home page card custamization and prevent zoom out in mobile devices

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   REST_URL: 'https://www.tecknocracy.com/api',
-  accessKeyId: 'AKIAJJJ7FYNWI5ETFNNA',
-  secretAccessKey: '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',
+  accessKeyId: 'AKIAJ3BCAYJR5WOSLTRQ',
+  secretAccessKey: '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',
   Bucket: 'tecknocracy',
   CLOUD_IMAGE_PATH: 'https://s3.amazonaws.com/tecknocracy',
   TINYMCE_API_KEY: 'sih8li4gqn3my2tprguwkz32a2hkakah8q55suzv2eubyiig'
