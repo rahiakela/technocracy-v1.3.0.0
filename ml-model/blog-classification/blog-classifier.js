033040be25d560dd81cade8609df5b7bd139ccd4
SahilBharti89 <saheel.bharti@yahoo.com>
2018-12-13 17:40:05 +0530

    adding machine learning documents to ML

--- ml-model/blog-classification/blog-classifier.js
+++ ml-model/blog-classification/blog-classifier.js
@@ -4,10 +4,12 @@ const classifier =new BayesClassifier();
 const fs = require('fs');
 const angularDocuments = require('./documents/angular-documents');
 const hibernateDocuments = require('./documents/hibernate-documents');
+const mlDocuments = require('./documents/ml-documents');
 
 // train the classifier
 classifier.addDocuments(angularDocuments.getAngularDocuments(), 'angular');
 classifier.addDocuments(hibernateDocuments.getHibernateDocuments(), 'hibernate');
+classifier.addDocuments(mlDocuments.getMLDocuments(),'machine-learning');
 
 // Create and store classifier model
 const classifierModel = `${__dirname}/blog-classifier-model.json`;
