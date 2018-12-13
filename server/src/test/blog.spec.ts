6b185a98045dbc8001a06446fd50b11917ab7d44
rahiakela <rahi.akela@yahoo.com>
2018-12-13 20:34:45 +0530

    blog test partial done

--- server/src/test/blog.spec.ts
+++ server/src/test/blog.spec.ts
@@ -6,6 +6,7 @@ chai.use(chaiHttp);
 
 const BASE_URL = 'http://localhost:3000/api/blog';
 const expect = chai.expect;
+const JWT_TOKEN = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTA0NWQ5NzM5MGJlYTNmOWM1MTQ3NzUiLCJlbWFpbCI6InZpZ25lcy5hcnVsam90aGlAZ21haWwuY29tIiwibmFtZSI6IlZpZ25lcyBBcnVsam90aGkiLCJleHAiOjE1NDUyMzQ1OTAsImlhdCI6MTU0NDYyOTc5MH0.OXv87OxQx9x2CM222HMML777JgZBOG3rOucwi7sRwdw`;
 
 describe('Blog API[/api/blog] Test Suite', () => {
 
@@ -38,12 +39,20 @@ describe('Blog API[/api/blog] Test Suite', () => {
 
     it('4 > Get Blog Written By Author API[/all/:writtenBy]: should return the list of blog', () => {
         chai.request(BASE_URL).get('/all/59945ee3d3a8604780c6e4a8')
-            .set('')
+            .set('x-access-token', JWT_TOKEN)
             .then(res => {
-                console.log(res.body);
                 expect(res.body[0].title).length.greaterThan(0);
                 expect(res.body[0].content).length.greaterThan(0);
-                expect(res.body[0].status).to.be.equal('published');
+            });
+    });
+
+    it('5 > Get Pending Blog List API[/all/pending/list]: should return the list of blog', () => {
+        chai.request(BASE_URL).get('/all/pending/list')
+            .set('x-access-token', JWT_TOKEN)
+            .then(res => {
+                expect(res.body[0].title).length.greaterThan(0);
+                expect(res.body[0].content).length.greaterThan(0);
+                expect(res.body[0].status).to.be.equal('pending');
             });
     });
 });
