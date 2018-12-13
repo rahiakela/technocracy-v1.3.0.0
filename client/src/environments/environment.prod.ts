e0ff202301ab297d533a308912715a06c6d5e3a0
rahiakela <rahi.akela@yahoo.com>
2018-12-13 20:54:14 +0530

    refectoring

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   REST_URL: 'https://www.tecknocracy.com/api',
-  accessKeyId: 'AKIAIA7MED6VTT7KJL7Q',
-  secretAccessKey: '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',
+  accessKeyId: '',
+  secretAccessKey: '',
   Bucket: 'tecknocracy',
   CLOUD_IMAGE_PATH: 'https://s3.amazonaws.com/tecknocracy',
   TINYMCE_API_KEY: 'sih8li4gqn3my2tprguwkz32a2hkakah8q55suzv2eubyiig'
